e06248e0de0e03f41c407de0c91d86318917f0d7
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-01-02 20:17:11 +0700

    Add MainActivity with Compose setup

--- app/src/main/java/com/example/pertemuan11/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan11/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan11
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,41 +9,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.pertemuan11.ui.theme.Pertemuan11Theme
+import com.example.pertemuan11.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // Menggunakan Pertemuan11Theme agar sesuai dengan namespace folder kamu
             Pertemuan11Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pertemuan11Theme {
-        Greeting("Android")
     }
 }
