cd6816ab2f1acd3408d58ab8aca9555db76f6906
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-01-02 20:21:21 +0700

    Add DestinasiHome route

---
+++ app/src/main/java/com/example/pertemuan11/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan11.view.route
+
+import com.example.pertemuan11.R
+import com.example.pertemuan11.view.route.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes= R.string.app_name
+}
