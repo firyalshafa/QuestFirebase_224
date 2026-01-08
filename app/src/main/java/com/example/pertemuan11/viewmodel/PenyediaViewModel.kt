5e2d1cc9c8ba6f349b366b44d15d2703f7426df8
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-01-08 18:41:47 +0700

    add CreationExtras extension to access AplikasiDataSiswa

--- app/src/main/java/com/example/pertemuan11/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/viewmodel/PenyediaViewModel.kt
@@ -1,20 +1,13 @@
 package com.example.pertemuan11.viewmodel
 
-
-
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.pertemuan11.repositori.AplikasiDataSiswa
-
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
-
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-    }
-}
