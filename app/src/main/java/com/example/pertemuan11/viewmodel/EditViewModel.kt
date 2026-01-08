1f0cde4689210e5e5bd2579b3d575524e187d6e2
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-01-08 18:39:18 +0700

    initialize EditViewModel state with student data

--- app/src/main/java/com/example/pertemuan11/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/viewmodel/EditViewModel.kt
@@ -18,4 +18,17 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
 RepositorySiswa
 ): ViewModel() {
 
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
 }
